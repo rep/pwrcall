6cf17ef563a71efbdc093ceb13821850376fb537
Mark Schloesser <ms@mwcollect.org>
2011-05-03 11:56:11 +0200

    updated setup.py

--- py/setup.py
+++ py/setup.py
@@ -15,18 +15,18 @@ if sys.version_info >= (3,):
 readme = read_file('README.md')
 
 setup(
-	name='evnet',
-	version = '1.0-4',
-	description='evnet is a networking library built on top of pyev (libev)',
+	name='pwrcall',
+	version = '1.0-1',
+	description='pwrcall is a framework for secure distributed function calls',
 	long_description=readme,
 	classifiers=['Development Status :: 4 - Beta','License :: OSI Approved :: MIT License','Programming Language :: Python','Topic :: Software Development :: Libraries :: Python Modules'], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
 	author='Mark Schloesser',
 	author_email='ms@mwcollect.org',
 	license = "MIT/BSD/GPL",
-	keywords = "evnet pyev network asynchronous nonblocking event",
-	url = "https://github.com/rep/evnet",
-	install_requires = ['pyev>=0.5.3-3.8'],
-	packages = ['evnet',],
+	keywords = "pwrcall evnet pyev network asynchronous nonblocking event rpc distributed",
+	url = "https://github.com/rep/pwrcall",
+	install_requires = ['evnet>=1.0-4'],
+	packages = ['pwrcall',],
 	**extra
 )
 
