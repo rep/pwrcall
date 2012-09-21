95620c2c1233ba917a4c7ae8b6fa6e9312d85026
rep <mark@etcho.de>
2012-09-21 12:57:32 +0200

    mathsrv example accepts cap as arg

--- py/examples/mathsrv.py
+++ py/examples/mathsrv.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -10,7 +11,10 @@ class Math(object):
 
 n = Node(cert='serverside.pem')
 m = Math()
-ref = n.register(m)
+if len(sys.argv) > 1:
+	ref = n.register(m, cap=sys.argv[1])
+else:
+	ref = n.register(m)
 n.listen(port=10000)
 print 'math obj ready at', n.refurl(ref)
 
